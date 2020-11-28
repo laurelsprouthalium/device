8761628df655ef29cd64951e8fd11e6faf935588
mint <him@mint.lgbt>
2020-11-28 13:21:21 -0300

    Make systemimage compile on Halium 9

--- aosp_laurel_sprout.mk
+++ aosp_laurel_sprout.mk
@@ -16,7 +16,8 @@ $(call inherit-product, device/xiaomi/aosp/device.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_GAPPS_ARCH := arm64
